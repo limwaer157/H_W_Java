71a0f4ba3b24be87dd6aa357efb9c1ec842efed7
Denis <Limwaer157@gmail.com>
2023-07-31 23:05:50 -0400

    Task_2

--- src/main/java/org/example/Home_work_Java_Exep/Seminar_2/Task_2/Main_Task_2.java
+++ src/main/java/org/example/Home_work_Java_Exep/Seminar_2/Task_2/Main_Task_2.java
@@ -1,4 +1,15 @@
 package org.example.Home_work_Java_Exep.Seminar_2.Task_2;
 
 public class Main_Task_2 {
+    public static void main(String[] args) {
+        int [] array = new int[9];
+        try {
+            int d = 0;
+            double catchedRes1 = (double) array[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
 }
